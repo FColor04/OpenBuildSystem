151d6fe475283e7d41d2718ffe6d3632978da0d4
R9_ <neamtrares@naver.com>
2024-04-28 20:30:53 +0300

    Exporting without need for project path

--- export.py
+++ export.py
@@ -3,10 +3,15 @@ from subprocess import run
 from os import chdir
 
 def export(path="TestProj/Test", platform='Windows'):
-    chdir(path)
-    with open("makefile.obs") as f:
+    p = path
+    if p.endswith("/"):
+        p += ""
+    else:
+        p += "/"
+    with open(p+'makefile.obs') as f:
         settings = json.loads(f.read())
     print(settings)
+    chdir(settings['proj_path'])
     args = ['godot', '--export-'+settings["exp_type"], f'{platform}', settings["exp_path"]]
     if bool(settings['headless']):
         args.append("--headless")
@@ -18,6 +23,7 @@ def write_settings(set_path="TestProj/Test/", export_type="release",
     settings["exp_path"] = export_path
     settings["exp_type"] = export_type
     settings['headless'] = headless
+    settings['proj_path'] = set_path
     p = set_path
     if p.endswith("/"):
         p += "makefile.obs"
@@ -27,5 +33,5 @@ def write_settings(set_path="TestProj/Test/", export_type="release",
         f.write(json.dumps(settings))
 
 if __name__ == "__main__":
-    write_settings(export_path='')
+    write_settings(export_path='C:/Users/R9_Dev/Documents/GitHub/OpenBuildSystem/TestProj/Test/test.exe')
     export()
