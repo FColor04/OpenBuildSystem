9b6eeeb052cbfd7fbc94ad501ff00e45e52c4ea3
Marcin Smoła <Marcinure1@gmail.com>
2024-04-28 22:04:05 +0200

    Tailwind based UI

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
+import threading
+import watchfiles
 import webview
+
+
+def watch_and_reload(window, event):
+    for change in watchfiles.watch('gui', stop_event=event):
+        # using this instead of window.load_url() because that didn't work for me
+        window.evaluate_js('window.location.reload()')
+        # window.load_url()
+
 
 if __name__ == '__main__':
     # Create a standard webview window
-    webview.create_window(
-        'Test', 'gui/index.html', confirm_close=True
+    window = webview.create_window(
+        'Open Build System', 'gui/index.html'
     )
-    webview.start()
+    # this handles stopping the watcher
+    thread_running = threading.Event()
+
+    # using a thread to watch
+    reload_thread = threading.Thread(
+        target=watch_and_reload,
+        args=(window, thread_running)
+    )
+    reload_thread.start()
+
+    # start the webview app
+    webview.start(debug=True)
+
+    # upon the webview app exitting, stop the watcher
+    thread_running.set()
+    reload_thread.join()
+
+    print('exitted successfully!')
--- settings.py
+++ settings.py
@@ -1,9 +1,14 @@
 import webview
 from exportlib import write_settings
 
+
+def on_start():
+    window.load_url("settings/index.html")
+
+
 if __name__ == '__main__':
     # Create a standard webview window
-    webview.create_window(
-        'Test', 'gui/settings/index.html', confirm_close=True
+    window = webview.create_window(
+        'Test', 'gui/index.html'
     )
-    webview.start()
+    webview.start(on_start, debug=True)
