4b84a260e4ed3c4c736ba518a63198854a77ea9a
R9_ <neamtrares@naver.com>
2024-04-28 20:22:46 +0300

    Headless

--- export.py
+++ export.py
@@ -7,13 +7,17 @@ def export(path="TestProj/Test", platform='Windows'):
     with open("makefile.obs") as f:
         settings = json.loads(f.read())
     print(settings)
-    run(['godot', '--export-'+settings["exp_type"], platform, settings["exp_path"]])
+    args = ['godot', '--export-'+settings["exp_type"], f'{platform}', settings["exp_path"]]
+    if bool(settings['headless']):
+        args.append("--headless")
+    run(args)
 
 def write_settings(set_path="TestProj/Test/", export_type="release",
-    export_path='absolute/path/to/project.exe'):
+    export_path='absolute/path/to/project.exe', headless=True):
     settings = {}
     settings["exp_path"] = export_path
     settings["exp_type"] = export_type
+    settings['headless'] = headless
     p = set_path
     if p.endswith("/"):
         p += "makefile.obs"
@@ -23,5 +27,5 @@ def write_settings(set_path="TestProj/Test/", export_type="release",
         f.write(json.dumps(settings))
 
 if __name__ == "__main__":
-    write_settings()
+    write_settings(export_path='')
     export()
