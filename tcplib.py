2177f362c708b03ec305c2818ca51861b83779f1
R9_ <neamtrares@naver.com>
2024-05-03 10:37:19 +0300

    LZMA compression

--- tcplib.py
+++ tcplib.py
@@ -1,25 +1,29 @@
 import easySocket as es
 import json
 import exportlib as ex
+import lzmalib as xz
 from subprocess import run
 from os import getcwd
 host = "127.0.0.1"
 port = 65432
 
-def start_client(platform="Windows"):
+def start_client(output="test.exe", platform="Windows"):
     data = {"platform": platform}
     conn = es.connect_tcp(host, port)
     es.send_text(json.dumps(data), conn)
     run("send/sendserver.exe")
+    xz.decompress_file("output.xz", output)
 
 def start_server(path="TestProj/Test"):
     conn = es.host_tcp(host, port)
     data = json.loads(es.rcv_data(conn).decode())["platform"]
     print(data)
     p = ex.export(path, data)
+    xz.compress_file(p, "output.xz")
     cwd = getcwd()
     print(cwd)
-    run(["sendclient.exe", str(conn.getsockname()[0]), p])
+    run(["E:\Rares\GitHub\OpenBuildSystem\sendclient.exe", str(conn.getsockname()[0]), "output.xz"])
     pass
 
-start_client()
+if __name__ == "__main__":
+    start_client()
